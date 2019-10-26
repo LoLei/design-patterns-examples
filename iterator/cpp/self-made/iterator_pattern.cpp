8a5d06d86a3eb01d1f0be0e4afccd6a0e4d60366
Lorenz Leitner <lorenz.leitner@student.tugraz.at>
2019-10-26 09:16:01 +0200

    Fix endless loop (copy pasta fail)

--- iterator/cpp/self-made/iterator_pattern.cpp
+++ iterator/cpp/self-made/iterator_pattern.cpp
@@ -8,6 +8,7 @@
 int main()
 {
   // Use custom array container
+  std::cout << "#### Array container" << std::endl;
   ArrayConcreteCollection my_custom_array;
   int items_to_add[5] = {1, 2, 3, 4, 5};
   my_custom_array.addItems(items_to_add);
@@ -20,25 +21,20 @@ int main()
   }
 
   // Use custom linked list container
+  std::cout << "#### Linked list container" << std::endl;
   LinkedListConcreteCollection my_custom_linked_list;
-  my_custom_linked_list.insert(1);
-  my_custom_linked_list.insert(2);
-  my_custom_linked_list.insert(3);
-  my_custom_linked_list.insert(4);
   my_custom_linked_list.insert(5);
-
-  std::cout << "LL insertion finished." << std::endl;
+  my_custom_linked_list.insert(4);
+  my_custom_linked_list.insert(3);
+  my_custom_linked_list.insert(2);
+  my_custom_linked_list.insert(1);
 
   Iterator<int>* my_custom_linked_list_iterator = my_custom_linked_list.createIterator();
-  std::cout << "Before loop" << std::endl;
   while(!my_custom_linked_list_iterator->isDone())
   {
-    std::cout << "Loop iteration" << std::endl;
     std::cout << my_custom_linked_list_iterator->currentItem() << std::endl;
-    my_custom_array_iterator->next();
+    my_custom_linked_list_iterator->next();
   }
-
-  std::cout << "LL traversal finished." << std::endl;
 
   return 0;
 }
