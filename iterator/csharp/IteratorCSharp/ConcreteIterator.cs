ac28a3209bf2317bd6f399b5264f99830ee8d096
Daniel Geiger <d4geiger@gmail.com>
2019-10-28 12:57:33 +0100

    add c# iterator example

---
+++ iterator/csharp/IteratorCSharp/ConcreteIterator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorCSharp
+{
+    abstract class Iterator
+    {
+        public abstract object First();
+        public abstract bool Next();
+        public abstract bool isDone();
+        public abstract object CurrentItem();
+    }
+    class ConcreteIterator :Iterator
+    {
+        private ConcreteArrayCollection _aggregate;
+        private int _current = -1;
+
+        public ConcreteIterator(ConcreteArrayCollection aggregate)
+        {
+            this._aggregate = aggregate;
+        }
+
+        public override object First()
+        {
+            return _aggregate[0];
+        }
+
+        public override bool Next()
+        {
+            _current++;
+            return _current <= _aggregate.Count;
+        }
+
+        public override bool isDone()
+        {
+            return _current <= _aggregate.Count;
+        }
+
+        public override object CurrentItem()
+        {
+            object ret = null;
+            if (_current < _aggregate.Count)
+            {
+                ret = _aggregate[_current];
+            }
+            return ret;
+        }
+    }
+}
---
+++ iterator/csharp/IteratorCSharp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcreteArrayCollection a = new ConcreteArrayCollection();
+            a[0] = 1;
+            a[1] = 2;
+            a[2] = 3;
+            a[3] = 4;
+            a[4] = 5;
+
+            Iterator i = a.CreateIterator();
+
+            while(i.Next())
+            {
+                Console.WriteLine(i.CurrentItem());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
