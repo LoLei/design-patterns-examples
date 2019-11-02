739ec7c731f098249b3be44aaf379bff686c956a
Daniel Geiger <d4geiger@gmail.com>
2019-11-02 15:17:03 +0100

    Add c# iterator example as dotnetcore project

---
+++ iterator/csharp/IteratorCSharpDotNetCore/CSharpIteratorDotNetCore/ConcreteCollection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpIteratorDotNetCore
+{
+    //Provides one or more methods for retrieving iterator instance, compatible with collection class
+    class ConcreteCollection : IteratorAggregate
+    {
+        private ArrayList _collection = new ArrayList();
+
+        public override IEnumerator GetEnumerator()
+        {
+            return new ConcreteIterator(this);
+        }
+
+        public ArrayList getItems()
+        {
+            return _collection;
+        }
+        public void AddItem(object item)
+        {
+            this._collection.Add(item);
+        }
+    }
+}
---
+++ iterator/csharp/IteratorCSharpDotNetCore/CSharpIteratorDotNetCore/Iterator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpIteratorDotNetCore
+{
+    abstract class Iterator : IEnumerator
+    {
+        object IEnumerator.Current => Current();
+        public abstract int Key();
+        public abstract object Current();
+        public abstract bool MoveNext();
+        public abstract void Reset();
+    }
+    abstract class IteratorAggregate : IEnumerable
+    {
+        public abstract IEnumerator GetEnumerator();
+    }
+}
