6d21b572359e987646b60c8bdcca787f30a9f145
Lorenz Leitner <lorenz.leitner@student.tugraz.at>
2019-11-04 14:50:32 +0100

    Add Python prototype example

---
+++ prototype/python/main.py
@@ -0,0 +1,73 @@
+"""
+Python already offers the cloning capability via the copy module, so you do
+not need to define the alternative prototype constructor for each class that
+implements the prototype
+"""
+import copy
+
+
+class Prototype:
+    """
+    This could e.g. be an abstract "Shape" class, while the concrete prototypes
+    below implement e.g. "Rectangle" and "Circle"
+    """
+    type_ = None
+    primitive_ = None
+
+    def clone(self):
+        pass
+
+    def get_info(self):
+        return self.type_, self.primitive_
+
+
+class ConcretePrototype1(Prototype):
+    def __init__(self, number):
+        self.type_ = "t1"
+        self.primitive_ = number
+
+    def clone(self):
+        """
+        This returns a new object with the members set to the primitives of the
+        source object, done in the copy module. Otherwise one would have to
+        create an alternate prototype constructor, which copies all the fields
+        """
+        return copy.copy(self)
+
+
+class ConcretePrototype2(Prototype):
+    def __init__(self, number):
+        self.type_ = "t2"
+        self.primitive_ = number
+
+    def clone(self):
+        return copy.copy(self)
+
+
+if __name__ == "__main__":
+    objects = []
+    object_t1_1 = ConcretePrototype1(314)
+    objects.append(object_t1_1)
+    object_t1_2 = object_t1_1.clone()
+    objects.append(object_t1_2)
+    object_t2_1 = ConcretePrototype2(1337)
+    objects.append(object_t2_1)
+    object_t2_2 = object_t2_1.clone()
+    objects.append(object_t2_2)
+
+    print("Manually created and cloned objects: ")
+    for concrete_prototype in objects:
+        t, p = concrete_prototype.get_info()
+        print("{}, {}".format(t, p))
+
+    # Now we will clone objects with different types, without even knowing what
+    # the types are. Working, because each concrete prototype implements its
+    # own clone() method
+    cloned_objects = []
+    for concrete_prototype in objects:
+        cloned_objects.append(concrete_prototype.clone())
+
+    print("Cloned objects without knowing exact types: ")
+    for concrete_prototype in cloned_objects:
+        t, p = concrete_prototype.get_info()
+        print("{}, {}".format(t, p))
