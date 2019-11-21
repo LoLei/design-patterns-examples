45eaa80a5669460ea5138b40156202ac18df3d53
Lorenz Leitner <lorenz.leitner@student.tugraz.at>
2019-11-21 11:49:48 +0100

    Use =default and =delete instead of private

--- raii/cpp/noncopyable.h
+++ raii/cpp/noncopyable.h
@@ -1,12 +1,15 @@
 /**
  * Prevents copying via private copy constructor 
- * and private assignment operator
+ * and private assignment operator (pre-C++11 explanation)
  */
 class NonCopyable 
 {
   public:
-    NonCopyable();
-  private:
-    NonCopyable(const NonCopyable &);
-    NonCopyable & operator = (const NonCopyable &);
+    // https://docs.microsoft.com/en-us/cpp/cpp/explicitly-defaulted-and-deleted-functions
+    // =default specifies to the compiler to generate the default body/implementation
+    NonCopyable() =default;
+
+    // =delete disables the function, similar to private (pre-C++11), but more thoroughly and efficiently
+    NonCopyable(const NonCopyable &) =delete;
+    NonCopyable & operator = (const NonCopyable &) =delete;
 };
