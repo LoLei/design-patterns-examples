322490dac346f4e106988bb240e513517ada459e
Sebastian Resch <sebastian.resch@student.tugraz.at>
2019-11-29 15:58:37 +0100

    Add python boilerplate/modules example

---
+++ python-boilerplate/module.py
@@ -0,0 +1,10 @@
+print('[module.py    ] Begin execution')
+print('[module.py    ] __name__ is:', __name__)
+
+def main():
+    print('[module.py    ] Running main function')
+
+if __name__ == '__main__':
+    main()
+
+print('[module.py    ] End execution')
---
+++ python-boilerplate/my-program.py
@@ -0,0 +1,12 @@
+import module
+
+print('[my-program.py] Begin execution')
+print('[my-program.py] __name__ is:', __name__)
+
+def main():
+    print('[my-program.py] Running main function')
+
+if __name__ == '__main__':
+    main()
+
+print('[my-program.py] End execution')
