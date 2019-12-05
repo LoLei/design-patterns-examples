38486ad3cb4f712e5eb4fce5cae46fbebc24b8d3
Daniel Geiger <d4geiger@gmail.com>
2019-12-05 18:26:52 +0100

    add Singleton c# example

---
+++ singleton/csharp/Singleton/Singleton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    class Singleton
+    {
+        // Eager Initialization, when class is loaded for first time. 
+        // Static means also thread safe
+        // With readonly value cannot be overwritten
+        private static readonly Singleton _instance = new Singleton();
+        public int member = 0;
+
+        protected Singleton()
+        {
+            //Hiding constructor
+        }
+
+        public static Singleton Instance()
+        {
+           return _instance;
+        }
+    }
+}
